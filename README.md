11fa244224174a0b6eaaedf7dcbde75d8ab5fdff
KlnSdr <schneider.klabs@gmail.com>
2020-11-01 12:19:44 +0100

    geleert

--- README.md
+++
@@ -1,4 +0,0 @@
-# kantiges-schulleben
-Eine wundervolle Seite für Schüler:Innen des Kantgymnasiums
-
-[zur Website](https://www.kantiges-schulleben.de)
--- shs/login/node.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-
-    if (isset($_SESSION['user']) === true && isset($_SESSION['typ']) === true) {
-        header("Location: ../index.php");
-    }
-    
-    echo file_get_contents("index.html");
-?>
