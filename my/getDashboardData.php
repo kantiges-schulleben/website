0debbd0d06395f07fb3f3a36cc04d841f5e58f89
KlnSdr <59069848+KlnSdr@users.noreply.github.com>
2020-11-07 16:51:41 +0100

    Rückgabe von "schulden" auskommentiert, da noch nicht implementiert

--- my/getDashboardData.php
+++ my/getDashboardData.php
@@ -2,7 +2,6 @@
     require_once "../include/functions.inc.php";
     
     if (isset($_SESSION["user"])) {
-        // TODO benötigte Daten auswählenund zurückgeben
         $result = SQL("SELECT * FROM handschlag WHERE name LIKE ?", [$_SESSION["user"]]);
 
         $output = array(
@@ -13,7 +12,7 @@
             array_push($output, array(
                 'fach' => $news['fach'],
                 "klasse" => $news['klasse'],
-                'schulden' => $news['schulden'],
+                // 'schulden' => $news['schulden'],
                 "me" => sqlReturn(SQL("SELECT name FROM benutzer WHERE id LIKE ?", [$news['id']]), 0, "name"),
                 "partner" => sqlReturn(SQL("SELECT name FROM benutzer WHERE id LIKE ?", [$news['partner']]), 0, "name")
             ));
