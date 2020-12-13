2512a091189e938dfc73f23d9729784aa1dc805c
KlnSdr <monsieurk1209@gmail.com>
2020-12-13 22:56:57 +0100

    * code und dateien der admin-Seite aufgeräumt

--- shs/admin/index.php
+++ shs/admin/index.php
@@ -2,7 +2,7 @@
     session_start();
     
     if (isset($_SESSION['userADMIN'])) {
-        echo file_get_contents("index.html");
+        echo file_get_contents("main.html");
     } else {
         header('Location: ./login/index.php');
     }
