ed185b265861169199592ace05d49a67dab5e38a
KlnSdr <monsieurk1209@gmail.com>
2021-03-08 20:16:11 +0100

    * weiterleitung zurück zu anmeldung, wenn über anmeldung

--- signup/index.php
+++ signup/index.php
@@ -2,7 +2,12 @@
     session_start();
 
     if (isset($_SESSION['user']) === true) {
+        if (isset($_SESSION["redirect"])) {
+            header("Location: ../" . $_SESSION['redirect']);
+            die();
+        }
         header("Location: ../");
+        die();
     }
     
     echo file_get_contents("./main.html");
