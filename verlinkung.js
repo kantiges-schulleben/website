48544c70112dbcbc3c85a7fe35ededaf66db92fa
MinoxTB <73794173+MinoxTB@users.noreply.github.com>
2021-03-20 14:18:33 +0100

    Add files via upload

---
+++ verlinkung.js
@@ -0,0 +1,12 @@
+
+function information () {
+    window.location="./information/main.html"
+}
+
+function registrierung () {
+    window.location="../signup/main.html"
+}
+
+function login () {
+    window.location="../login/main.html"
+}
