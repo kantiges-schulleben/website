2297ddd1e24e345754a82e17b4554b99a746a2ff
KlnSdr <59069848+KlnSdr@users.noreply.github.com>
2021-11-27 18:24:55 +0100

    codesäuberung russian style

--- shs/admin/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    
-    if (isset($_SESSION['userADMIN'])) {
-        echo file_get_contents("main.html");
-    } else {
-        header('Location: ./login/index.php');
-    }
-?>
--- shs/admin/login/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if (isset($_SESSION['userADMIN'])) {
-        header("Location:../");
-        die();
-    }
-    echo file_get_contents("main.html");
-?>
